7bfde315f3da06622064d526bef85bbe14c0d177
haiko van der schaaf <hvdschaaf@gmail.com>
2023-10-30 13:58:50 +0100

    add test

---
+++ alfresco-authentication/backend/plugin/src/test/kotlin/com/ritense/valtimo/alfresco/AlfrescoAuthenticationPluginTest.kt
@@ -0,0 +1,36 @@
+package com.ritense.valtimo.alfresco
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.mockito.kotlin.*
+import org.springframework.http.HttpMethod
+import org.springframework.web.reactive.function.client.ClientRequest
+import org.springframework.web.reactive.function.client.ClientResponse
+import org.springframework.web.reactive.function.client.ExchangeFunction
+import reactor.core.publisher.Mono
+import java.net.URI
+
+internal class AlfrescoAuthenticationPluginTest {
+
+    @Test
+    fun shouldAddToken() {
+        val requestCaptor = argumentCaptor<ClientRequest>()
+        val tokenGeneratorService = mock<AlfrescoTokenGeneratorService>()
+        val plugin = AlfrescoAuthenticationPlugin   (tokenGeneratorService)
+        plugin.clientId = "test"
+        plugin.clientSecret = "test"
+
+        whenever(tokenGeneratorService.generateToken("test", "test")).thenReturn("token")
+
+        val request = ClientRequest.create(HttpMethod.GET, URI.create("http://some-url.tld")).build()
+        val nextFilter = mock<ExchangeFunction>()
+        val response = mock<Mono<ClientResponse>>()
+        whenever(nextFilter.exchange(any())).thenReturn(response)
+
+        plugin.filter(request, nextFilter)
+
+        verify(nextFilter).exchange(requestCaptor.capture())
+        assertEquals("Bearer token", requestCaptor.firstValue.headers()["Authorization"]?.get(0))
+    }
+}

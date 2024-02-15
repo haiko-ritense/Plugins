b7fe29695967d4d39b04c7c7ca077809a121c04a
huibvanstraten <huib.van.straten@ritense.com>
2024-02-15 12:43:31 +0100

    WIP

--- publictask/backend/plugin/src/main/kotlin/com/ritense/valtimo/backend/plugin/service/PublicTaskService.kt
+++ publictask/backend/plugin/src/main/kotlin/com/ritense/valtimo/backend/plugin/service/PublicTaskService.kt
@@ -1,5 +1,7 @@
 package com.ritense.valtimo.backend.plugin.service
 
+import com.ritense.document.service.DocumentService
+import com.ritense.formlink.service.impl.CamundaFormAssociationService
 import com.ritense.valtimo.backend.plugin.domain.PublicTaskEntity
 import mu.KotlinLogging
 import org.camunda.bpm.engine.RuntimeService
@@ -8,9 +10,12 @@ import org.camunda.bpm.engine.delegate.DelegateTask
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import java.util.Optional
 
 class PublicTaskService(
     private val runtimeService: RuntimeService,
+    private val formAssociationService: CamundaFormAssociationService,
+    private val documentService: DocumentService
 ) {
 
     @Value("\${valtimo.url}") private lateinit var baseUrl: String
@@ -33,16 +38,25 @@ class PublicTaskService(
         execution.setVariable("url", publicTaskUrl)
     }
 
-    fun createPublicTaskHtml(taskUuid: String): ResponseEntity<String> {
+    fun createPublicTaskHtml(
+        taskUuid: String,
+        businessKey: String
+    ): ResponseEntity<String> {
         // TODO: build it ;)
 
         // step 1: get entity and get the userTaskId
 
-        //step 2: get form.io json with the userTaskId
+        // step 2: check if task (still) exists, if not return a html with message: "task does not exist"
 
-        //step 3: check if task (still) exists
+        // step 3: get form.io json with the userTaskId
+        val formKey = ""
 
-        // step 4: if exists-render HTML with the form.io else-send response with message: "task does not exist"
+        // step 4: get prefilled data from ?
+        val document = Optional.of(documentService.get(businessKey).id())
+        val prefilledFormIoForm = formAssociationService.getPreFilledFormDefinitionByFormKey(formKey, document)
+
+        // step 5: render HTML with the form.io
+
         val formHtml = """<!DOCTYPE html>
 <html xmlns:th="http://www.thymeleaf.org">
 <head>

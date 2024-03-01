ba6c0af66fb42aa3ad3a26d386d0e25ff31777a6
Huib van Straten <huib.van.straten@ritense.com>
2024-03-01 11:48:36 +0100

    Update publictask/backend/plugin/src/main/kotlin/com/ritense/valtimo/backend/plugin/htmlrenderer/config/FreemarkerConfig.kt
    Co-authored-by: Klaas Schuijtemaker <94360980+Klaas-Ritense@users.noreply.github.com>

--- publictask/backend/plugin/src/main/kotlin/com/ritense/valtimo/backend/plugin/htmlrenderer/config/FreemarkerConfig.kt
+++ publictask/backend/plugin/src/main/kotlin/com/ritense/valtimo/backend/plugin/htmlrenderer/config/FreemarkerConfig.kt
@@ -6,7 +6,7 @@ import freemarker.template.TemplateExceptionHandler
 
 class FreemarkerConfig : Configuration(VERSION_2_3_31) {
     init {
-        templateLoader = ClassTemplateLoader(javaClass, "/config/form")
+        templateLoader = ClassTemplateLoader(javaClass, "/config/template")
         defaultEncoding = Charsets.UTF_8.toString()
         templateExceptionHandler = TemplateExceptionHandler.RETHROW_HANDLER
     }

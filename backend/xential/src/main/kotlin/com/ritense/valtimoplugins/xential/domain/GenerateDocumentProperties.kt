f150b600678aa300a9a3ec0643c2a59eafb2c355
paul-ritense <paul.van.beukering@ritense.com>
2024-12-27 16:00:41 +0100

    clean up code

--- backend/xential/src/main/kotlin/com/ritense/valtimoplugins/xential/domain/GenerateDocumentProperties.kt
+++ backend/xential/src/main/kotlin/com/ritense/valtimoplugins/xential/domain/GenerateDocumentProperties.kt
@@ -1,6 +1,5 @@
 package com.ritense.valtimoplugins.xential.domain
 
-import com.ritense.valtimoplugins.xential.plugin.TemplateDataEntry
 import java.util.UUID
 
 data class GenerateDocumentProperties(

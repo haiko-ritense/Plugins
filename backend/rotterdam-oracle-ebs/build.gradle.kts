3db036488c87659ada84bc1c9f887dedbba08e4e
Maarten van Toor <maarten.van.toor@ritense.com>
2025-02-07 15:10:23 +0100

    removed commented out dependencies

--- backend/rotterdam-oracle-ebs/build.gradle.kts
+++ backend/rotterdam-oracle-ebs/build.gradle.kts
@@ -38,9 +38,6 @@ dependencies {
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
 
     // Jackson FasterXML
-//    implementation("com.fasterxml.jackson.core:jackson-core")
-//    implementation("com.fasterxml.jackson.core:jackson-databind")
-//    implementation("com.fasterxml.jackson.core:jackson-annotations")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     // Apache HTTP Client
